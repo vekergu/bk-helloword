043e241a21fec75a8be84b0febcb602d9ff415b9
gushaofeng1 <gushaofeng1@crnet.com.hk>
2018-10-10 10:41:15 +0800

    变更初始信息

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -20,9 +20,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                        # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
+        'USER': 'test',                        # 你的数据库user
+        'PASSWORD': 'test',                        # 你的数据库password
+        'HOST': '192.168.36.3',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
